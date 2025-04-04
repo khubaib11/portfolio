8867c5356921ac4958c07623e93bf8b5196e64f0
khubaib khan <khankhubaib089@gmail.com>
2025-04-04 12:32:39 +0500

    add deploy functionality

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    host: '0.0.0.0', // Allows access from any device in the same network
-    port: 5174, // Default Vite port (change if needed)
-    open: true, // Automatically open the app in the browser  }
-  }
+  base:"/portfolio",
 })
